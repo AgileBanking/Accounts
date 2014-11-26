70aaf276524bbad1a8ce876c4cfd176d61c11eb0
Nikos Karamolegos <ncaramolegos@gmail.com>
2014-11-26 10:43:00 +0200

    In Grails 2.3.8 and Remote database

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,43 +1,46 @@
 dataSource {
-    //h2
+    // h2
 //    pooled = true
 //    driverClassName = "org.h2.Driver"
 //    username = "sa"
 //    password = ""
-    
+
     // MySQL
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
-    username = "root" 
-    password  = "agiler"      
+    username = "client" 
+    password = "agiler" 
+
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
 }
+
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', '', 'create-drop'
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
 //            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            url = "jdbc:mysql://localhost:3306/accounts" 
+//            url = "jdbc:mysql://localhost:3306/accounts"   
+            url = "jdbc:mysql://accounts.db:3306/accounts?useUnicode=yes&characterEncoding=UTF-8" 
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-//            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            url = "jdbc:mysql://localhost:3306/accounts" 
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            url = "jdbc:mysql://localhost:3306/commons" 
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            url = "jdbc:mysql://localhost:3306/accounts" 
-            pooled = true
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            url = "jdbc:mysql://localhost:3306/commons" 
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
